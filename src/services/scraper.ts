96919c814f4e3be3a5165bc25c640aa604251af7
suryG <sury0504172975@gmail.com>
2026-08-06 23:28:25 +0300

    fix: install full puppeteer package

--- src/services/scraper.ts
+++ src/services/scraper.ts
@@ -92,9 +92,10 @@ export async function fetchAndProcessTransactions() {
       browserOptions: {
         args: [
           "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-dev-shm-usage",
-          "--disable-gpu",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-accelerated-2d-canvas",
+    "--disable-gpu",
         ],
       },
     };
